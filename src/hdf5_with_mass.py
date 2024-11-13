d10a403a50b22ffd74568bd70054ad38ce2f4532
finn-pal <173330723+finn-pal@users.noreply.github.com>
2024-11-14 09:34:24 +1100

    Set up multiprocessing to improve function run time.

--- src/hdf5_with_mass.py
+++ src/hdf5_with_mass.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import multiprocessing as mp
 import os
 import sys
 
@@ -10,30 +11,14 @@ from gc_utils import iteration_name  # type: ignore
 from tools.get_mass_at_snap import get_gc_masses_at_snap
 
 
-def main(simulation: str, iteration_low_limit: int, iteration_up_limit: int, location: str):
-    if location == "local":
-        sim_dir = "/Users/z5114326/Documents/simulations/"
-        data_dir = "/Users/z5114326/Documents/GitHub/gc_process_katana/data/"
-        sim_codes = data_dir + "external/simulation_codes.json"
-        model_snaps = data_dir + "external/model_snapshots.json"
-
-    elif location == "katana":
-        data_dir = "/srv/scratch/astro/z5114326/gc_process/data/"
-        sim_dir = "/srv/scratch/astro/z5114326/simulations/"
-        sim_codes = data_dir + "external/simulation_codes.json"
-        model_snaps = data_dir + "external/model_snapshots.json"
-
-    else:
-        print("Incorrect location provided. Must be local or katana.")
-        sys.exit()
-
+def create_hdf5(simulation: str, it_lst: list[int], sim_dir: str, data_dir: str):
     save_file = sim_dir + simulation + "/" + simulation + "_processed.hdf5"  # save location
 
     if not os.path.exists(save_file):
         h5py.File(save_file, "w")
 
     with h5py.File(save_file, "a") as hdf:
-        for it in range(iteration_low_limit, iteration_up_limit + 1):
+        for it in it_lst:
             it_id = iteration_name(it)
             data_file = data_dir + "results/" + simulation + "/interim/" + it_id + ".json"
             with open(data_file, "r") as sim_json:
@@ -58,20 +43,35 @@ def main(simulation: str, iteration_low_limit: int, iteration_up_limit: int, loc
 
     hdf.close()
 
-    # start adding masses at all available snapshots
-    with open(sim_codes) as sim_json:
-        sim_data = json.load(sim_json)
 
-    with open(model_snaps) as snap_json:
-        snap_data = json.load(snap_json)
+def add_mass_hdf5(simulation, it_lst: list[int], result_dict: dict, sim_dir: str):
+    proc_file = sim_dir + simulation + "/" + simulation + "_processed.hdf5"
+    proc_data = h5py.File(proc_file, "a")  # open processed data file
 
-    snap_offset = sim_data[simulation]["offset"]
-    snap_lst = snap_data["public_snapshots"]
+    for it in it_lst:
+        it_id = iteration_name(it)
+        if it_id in proc_data.keys():
+            it_grouping = proc_data[it_id]
+        else:
+            it_grouping = proc_data.create_group(it_id)
+        if "snapshots" in it_grouping.keys():
+            snap_groups = it_grouping["snapshots"]
+        else:
+            snap_groups = it_grouping.create_group("snapshots")
+        for snap_id in result_dict[it_id].keys():
+            if snap_id in snap_groups.keys():
+                snapshot = snap_groups[snap_id]
+            else:
+                snapshot = snap_groups.create_group(snap_id)
+                for key in result_dict[it_id][snap_id].keys():
+                    if key in snapshot.keys():
+                        del snapshot[key]
+                    if key == "ptype":
+                        snapshot.create_dataset(key, data=result_dict[it_id][snap_id][key])
+                    else:
+                        snapshot.create_dataset(key, data=np.array(result_dict[it_id][snap_id][key]))
 
-    it_rng = iteration_up_limit - iteration_low_limit
-    it_lst = np.linspace(iteration_low_limit, iteration_up_limit, it_rng + 1, dtype=int)
-
-    get_gc_masses_at_snap(simulation, snap_offset, it_lst, snap_lst, sim_dir, data_dir)
+    proc_data.close()
 
 
 if __name__ == "__main__":
@@ -80,6 +80,56 @@ if __name__ == "__main__":
     parser.add_argument("-a", "--iteration_low_limit", required=True, type=int, help="lower bound iteration")
     parser.add_argument("-b", "--iteration_up_limit", required=True, type=int, help="upper bound iteration")
     parser.add_argument("-l", "--location", required=True, type=str, help="either local or katana")
+    parser.add_argument("-c", "--cores", required=False, type=int, help="number of cores to run process on")
     args = parser.parse_args()
 
-    main(args.simulation, args.iteration_low_limit, args.iteration_up_limit, args.location)
+    it_up = args.iteration_up_limit
+    it_low = args.iteration_low_limit
+    it_rng = it_up - it_low
+    it_lst = np.linspace(it_low, it_up, it_rng + 1, dtype=int)
+
+    sim = args.simulation
+    location = args.location
+
+    if location == "local":
+        sim_dir = "/Users/z5114326/Documents/simulations/"
+        data_dir = "/Users/z5114326/Documents/GitHub/gc_process_katana/data/"
+        sim_codes = data_dir + "external/simulation_codes.json"
+        model_snaps = data_dir + "external/model_snapshots.json"
+
+    elif location == "katana":
+        data_dir = "/srv/scratch/astro/z5114326/gc_process/data/"
+        sim_dir = "/srv/scratch/astro/z5114326/simulations/"
+        sim_codes = data_dir + "external/simulation_codes.json"
+        model_snaps = data_dir + "external/model_snapshots.json"
+
+    else:
+        print("Incorrect location provided. Must be local or katana.")
+        sys.exit()
+
+    create_hdf5(sim, it_lst, sim_dir, data_dir)
+
+    # start adding masses at all available snapshots
+    with open(sim_codes) as sim_json:
+        sim_data = json.load(sim_json)
+
+    with open(model_snaps) as snap_json:
+        snap_data = json.load(snap_json)
+
+    snap_offset = sim_data[sim]["offset"]
+    snap_lst = snap_data["analyse_snapshots"]
+
+    cores = args.cores
+    if cores is None:
+        cores = mp.cpu_count()
+
+    with mp.Manager() as manager:
+        shared_dict = manager.dict()  # Shared dictionary across processes
+        args = [(sim, snap_offset, it, snap_lst, sim_dir, data_dir, shared_dict) for it in it_lst]
+
+        with mp.Pool(processes=cores, maxtasksperchild=1) as pool:
+            pool.starmap(get_gc_masses_at_snap, args, chunksize=1)
+
+        final_dict = dict(shared_dict)
+
+    add_mass_hdf5(sim, it_lst, final_dict, sim_dir)
--- src/tools/get_mass_at_snap.py
+++ src/tools/get_mass_at_snap.py
@@ -13,93 +13,82 @@ from gc_utils import iteration_name, particle_type, snapshot_name  # type: ignor
 def get_gc_masses_at_snap(
     simulation: str,
     offset: int,
-    iteration_list: list[int],
+    it: int,
     snapshot_list: list[int],
     sim_dir: str,
     data_dir: str,
+    data_dict: dict,
 ):
     proc_file = sim_dir + simulation + "/" + simulation + "_processed.hdf5"
-    proc_data = h5py.File(proc_file, "a")  # open processed data file
+    proc_data = h5py.File(proc_file, "r")  # open processed data file
 
-    for it in iteration_list:
-        raw_dir = data_dir + "results/" + simulation + "/raw/it_%d/" % it
-        it_id = iteration_name(it)
+    it_id = iteration_name(it)
 
-        gc_id_lst = proc_data[it_id]["source"]["gc_id"]
-        analyse_flag_lst = proc_data[it_id]["source"]["analyse_flag"]
-        group_id_lst = proc_data[it_id]["source"]["group_id"]
-        snap_acc_lst = proc_data[it_id]["source"]["snap_acc"]
+    # data_dict[it_id] = {}
 
-        for snapshot in snapshot_list:
-            snap_id = snapshot_name(snapshot)
+    raw_dir = data_dir + "results/" + simulation + "/raw/it_%d/" % it
 
-            snap_mass_file = raw_dir + "allcat_s-%d_p2-7_p3-1_k-1.5_logm_snap%d.txt" % (it, snapshot - offset)
-            gc_id_file = raw_dir + "allcat_s-%d_p2-7_p3-1_gcid.txt" % it
+    gc_id_lst = proc_data[it_id]["source"]["gc_id"]
+    analyse_flag_lst = proc_data[it_id]["source"]["analyse_flag"]
+    group_id_lst = proc_data[it_id]["source"]["group_id"]
+    snap_acc_lst = proc_data[it_id]["source"]["snap_acc"]
 
-            gcid_df = pd.read_csv(gc_id_file, sep=" ").drop(["ID"], axis=1)
-            gcid_df.columns = ["GC_ID", "quality"]
+    snap_dict = {}
+    for snapshot in snapshot_list:
+        snap_id = snapshot_name(snapshot)
 
-            mass_snap_df = pd.read_csv(snap_mass_file, header=None)
-            mass_snap_df.columns = ["mass"]
+        snap_mass_file = raw_dir + "allcat_s-%d_p2-7_p3-1_k-1.5_logm_snap%d.txt" % (it, snapshot - offset)
+        gc_id_file = raw_dir + "allcat_s-%d_p2-7_p3-1_gcid.txt" % it
 
-            comb_df = pd.concat([gcid_df, mass_snap_df], axis=1)
-            filt_comb_df = comb_df[comb_df["mass"] != -1]
+        gcid_df = pd.read_csv(gc_id_file, sep=" ").drop(["ID"], axis=1)
+        gcid_df.columns = ["GC_ID", "quality"]
 
-            gc_id_snap = [gc_id for gc_id in filt_comb_df["GC_ID"]]
-            mass_snap = [mass for mass in filt_comb_df["mass"]]
-            ptype_snap = [particle_type(quality) for quality in filt_comb_df["quality"]]
-            idx_lst = filt_comb_df.index
+        mass_snap_df = pd.read_csv(snap_mass_file, header=None)
+        mass_snap_df.columns = ["mass"]
 
-            snap_accr_snap = []
-            group_id_snap = []
-            gc_test_set = []
-            analyse_flag_snap = []
+        comb_df = pd.concat([gcid_df, mass_snap_df], axis=1)
+        filt_comb_df = comb_df[comb_df["mass"] != -1]
 
-            for idx in idx_lst:
-                analyse_flag = analyse_flag_lst[idx]
-                snap_accr = snap_acc_lst[idx]
-                group_id = group_id_lst[idx]
-                gc = gc_id_lst[idx]
+        gc_id_snap = [gc_id for gc_id in filt_comb_df["GC_ID"]]
+        mass_snap = [mass for mass in filt_comb_df["mass"]]
+        ptype_snap = [particle_type(quality) for quality in filt_comb_df["quality"]]
+        idx_lst = filt_comb_df.index
 
-                analyse_flag_snap.append(analyse_flag)
-                snap_accr_snap.append(snap_accr)
-                group_id_snap.append(group_id)
-                gc_test_set.append(gc)
+        snap_acc_snap = []
+        group_id_snap = []
+        gc_test_set = []
+        analyse_flag_snap = []
 
-            for gc in gc_id_snap:
-                if gc not in gc_test_set:
-                    sys.exit("GC Number mismatch")
+        for idx in idx_lst:
+            analyse_flag = analyse_flag_lst[idx]
+            snap_accr = snap_acc_lst[idx]
+            group_id = group_id_lst[idx]
+            gc = gc_id_lst[idx]
 
-            for gc in gc_test_set:
-                if gc not in gc_id_snap:
-                    sys.exit("GC Number mismatch")
+            analyse_flag_snap.append(analyse_flag)
+            snap_acc_snap.append(snap_accr)
+            group_id_snap.append(group_id)
+            gc_test_set.append(gc)
 
-            data_dict = {
-                "gc_id": gc_id_snap,
-                "ptype": ptype_snap,
-                "group_id": group_id_snap,
-                "accr_snap": snap_accr_snap,
-                "mass": mass_snap,
-                "analyse_flag": analyse_flag_snap,
-            }
+        for gc in gc_id_snap:
+            if gc not in gc_test_set:
+                sys.exit("GC Number mismatch")
 
-            # now add masses at each snapshot to file.
-            if it_id in proc_data.keys():
-                grouping = proc_data[it_id]
-            else:
-                grouping = proc_data.create_group(it_id)
-            if "snapshots" in grouping.keys():
-                kinematics = grouping["snapshots"]
-            else:
-                kinematics = grouping.create_group("snapshots")
-            if snap_id in kinematics.keys():
-                snap_group = kinematics[snap_id]
-            else:
-                snap_group = kinematics.create_group(snap_id)
-            for key in data_dict.keys():
-                if key in snap_group.keys():
-                    del snap_group[key]
-                if key == "ptype":
-                    snap_group.create_dataset(key, data=data_dict[key])
-                else:
-                    snap_group.create_dataset(key, data=np.array(data_dict[key]))
+        for gc in gc_test_set:
+            if gc not in gc_id_snap:
+                sys.exit("GC Number mismatch")
+
+        key_dict = {
+            "gc_id": gc_id_snap,
+            "ptype": ptype_snap,
+            "group_id": group_id_snap,
+            "acc_snap": snap_acc_snap,
+            "mass": mass_snap,
+            "analyse_flag": analyse_flag_snap,
+        }
+
+        snap_dict[snap_id] = key_dict
+
+    data_dict[it_id] = snap_dict
+
+    return data_dict
