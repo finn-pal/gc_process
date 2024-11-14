be8a44ff3c586fbc45a749a56c8ea8819945542d
finn-pal <173330723+finn-pal@users.noreply.github.com>
2024-11-15 09:35:42 +1100

    Get basic kinematics ready for upload to katana.

---
+++ src/basic_kinematics.py
@@ -0,0 +1,111 @@
+import argparse
+import json
+import multiprocessing as mp
+
+import h5py
+import numpy as np
+from gc_utils import iteration_name, open_snapshot, snapshot_name  # type: ignore
+
+from tools.get_basic_kinematics_at_snap import get_basic_kinematics
+
+
+def main(
+    simulation: str,
+    it_lst: list[int],
+    snapshot: int,
+    sim_dir: str,
+    shared_dict: dict = {},
+):
+    fire_dir = sim_dir + simulation + "/" + simulation + "_res7100/"
+
+    part = open_snapshot(snapshot, fire_dir)
+    get_basic_kinematics(part, simulation, it_lst, snapshot, sim_dir, shared_dict)
+
+
+def add_kinematics_hdf5(simulation, it_lst: list[int], snap_lst: list[int], result_dict: dict, sim_dir: str):
+    proc_file = sim_dir + simulation + "/" + simulation + "_processed.hdf5"
+    proc_data = h5py.File(proc_file, "a")  # open processed data file
+
+    for it in it_lst:
+        it_id = iteration_name(it)
+        if it_id in proc_data.keys():
+            it_grouping = proc_data[it_id]
+        else:
+            it_grouping = proc_data.create_group(it_id)
+        if "snapshots" in it_grouping.keys():
+            snap_groups = it_grouping["snapshots"]
+        else:
+            snap_groups = it_grouping.create_group("snapshots")
+        for snap in snap_lst:
+            snap_id = snapshot_name(snap)
+            if snap_id in snap_groups.keys():
+                snapshot = snap_groups[snap_id]
+            else:
+                snapshot = snap_groups.create_group(snap_id)
+            for key in result_dict[snap_id][it_id].keys():
+                if key in snapshot.keys():
+                    del snapshot[key]
+                else:
+                    snapshot.create_dataset(key, data=result_dict[snap_id][it_id][key])
+
+    proc_data.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--simulation", required=True, type=str, help="simulation name (e.g. m12i)")
+    parser.add_argument("-a", "--iteration_low_limit", required=True, type=int, help="lower bound iteration")
+    parser.add_argument("-b", "--iteration_up_limit", required=True, type=int, help="upper bound iteration")
+    parser.add_argument("-l", "--location", required=True, type=str, help="either local or katana")
+    parser.add_argument("-c", "--cores", required=False, type=int, help="number of cores to run process on")
+    parser.add_argument(
+        "-n",
+        "--snapshots",
+        required=False,
+        nargs="+",
+        type=int,
+        help="list of snapshots of interest, if None provided will default to all publicly available",
+    )
+
+    args = parser.parse_args()
+
+    it_min = args.iteration_low_limit
+    it_max = args.iteration_up_limit
+    it_lst = np.linspace(it_min, it_max, it_max - it_min + 1, dtype=int)
+
+    sim = args.simulation
+    location = args.location
+
+    if location == "local":
+        sim_dir = "/Users/z5114326/Documents/simulations/"
+        data_dir = "/Users/z5114326/Documents/GitHub/gc_process_katana/data/"
+        model_snaps = data_dir + "external/model_snapshots.json"
+
+    elif location == "katana":
+        data_dir = "/srv/scratch/astro/z5114326/gc_process/data/"
+        sim_dir = "/srv/scratch/astro/z5114326/simulations/"
+        model_snaps = data_dir + "external/model_snapshots.json"
+
+    with open(model_snaps) as snap_json:
+        snap_data = json.load(snap_json)
+
+    snap_lst = args.snapshots
+    if snap_lst is None:
+        snap_lst = snap_data["analyse_snapshots"]
+
+    cores = args.cores
+    if cores is None:
+        # 4 cores is max to run with 64 GB RAM
+        cores = mp.cpu_count()
+
+    with mp.Manager() as manager:
+        shared_dict = manager.dict()  # Shared dictionary across processes
+        # args = [(sim, it_lst, snap_group, sim_dir, data_dir, shared_dict) for snap_group in snap_groups]
+        args = [(sim, it_lst, snap, sim_dir, shared_dict) for snap in snap_lst]
+
+        with mp.Pool(processes=cores, maxtasksperchild=1) as pool:
+            pool.starmap(main, args, chunksize=1)
+
+        result_dict = dict(shared_dict)
+
+    add_kinematics_hdf5(sim, it_lst, snap_lst, result_dict, sim_dir)
---
+++ src/tools/get_basic_kinematics_at_snap.py
@@ -0,0 +1,128 @@
+import h5py
+import numpy as np
+import utilities as ut
+from gc_utils import iteration_name, snapshot_name  # type: ignore
+
+
+def get_basic_kinematics(
+    part,
+    sim: str,
+    it_lst: list[int],
+    snapshot: int,
+    sim_dir: str,
+    data_dict: dict = {},
+    host_index: int = 0,
+):
+    snap_id = snapshot_name(snapshot)
+
+    proc_file = sim_dir + sim + "/" + sim + "_processed.hdf5"
+    proc_data = h5py.File(proc_file, "r")  # open processed data file
+
+    it_dict = {}
+    for it in it_lst:
+        it_id = iteration_name(it)
+
+        gc_id_snap = proc_data[it_id]["snapshots"][snap_id]["gc_id"]
+
+        if len(gc_id_snap) is None:
+            continue
+
+        ptype_byte_snap = proc_data[it_id]["snapshots"][snap_id]["ptype"]
+        ptype_snap = [ptype.decode("utf-8") for ptype in ptype_byte_snap]
+
+        host_name = ut.catalog.get_host_name(host_index)
+
+        x_lst = []
+        y_lst = []
+        z_lst = []
+        vx_lst = []
+        vy_lst = []
+        vz_lst = []
+
+        r_cyl_lst = []
+        phi_cyl_lst = []
+        vr_cyl_lst = []
+        vphi_cyl_lst = []
+
+        init_cond_lst = []
+
+        r_lst = []
+
+        ep_fire_lst = []
+        ek_lst = []
+
+        lx_lst = []
+        ly_lst = []
+        lz_lst = []
+
+        for gc, ptype in zip(gc_id_snap, ptype_snap):
+            idx = np.where(part[ptype]["id"] == gc)[0][0]
+            pos_xyz = part[ptype].prop(f"{host_name}.distance.principal", idx)
+            vel_xyz = part[ptype].prop(f"{host_name}.velocity.principal", idx)
+
+            pos_cyl = part[ptype].prop(f"{host_name}.distance.principal.cylindrical", idx)
+            vel_cyl = part[ptype].prop(f"{host_name}.velocity.principal.cylindrical", idx)
+
+            init_cond = np.concatenate((pos_xyz, vel_xyz))
+
+            ep_fir = part[ptype]["potential"][idx]
+            ek = 0.5 * np.linalg.norm(vel_xyz) ** 2
+
+            x, y, z = pos_xyz
+            vx, vy, vz = vel_xyz
+
+            r_cyl, phi_cyl, _ = pos_cyl
+            vr_cyl, vphi_cyl, _ = vel_cyl
+
+            r = np.linalg.norm(pos_xyz)
+
+            lx = y * vz - z * vy
+            ly = z * vx - x * vz
+            lz = x * vy - y * vx
+
+            x_lst.append(x)
+            y_lst.append(y)
+            z_lst.append(z)
+            vx_lst.append(vx)
+            vy_lst.append(vy)
+            vz_lst.append(vz)
+
+            r_cyl_lst.append(r_cyl)
+            phi_cyl_lst.append(phi_cyl)
+            vr_cyl_lst.append(vr_cyl)
+            vphi_cyl_lst.append(vphi_cyl)
+
+            r_lst.append(r)
+
+            ep_fire_lst.append(ep_fir)
+            ek_lst.append(ek)
+
+            lx_lst.append(lx)
+            ly_lst.append(ly)
+            lz_lst.append(lz)
+
+            init_cond_lst.append(init_cond)
+
+        kin_dict = {
+            "x": np.array(x_lst),
+            "y": np.array(y_lst),
+            "z": np.array(z_lst),
+            "vx": np.array(vx_lst),
+            "vy": np.array(vy_lst),
+            "vz": np.array(vz_lst),
+            "r_cyl": np.array(r_cyl_lst),
+            "phi_cyl": np.array(phi_cyl_lst),
+            "vr_cyl": np.array(vr_cyl_lst),
+            "vphi_cyl": np.array(vphi_cyl_lst),
+            "r": np.array(r_lst),
+            "ep_fire": np.array(ep_fire_lst),
+            "ek": np.array(ek_lst),
+            "lx": np.array(lx_lst),
+            "ly": np.array(ly_lst),
+            "lz": np.array(lz_lst),
+        }
+
+        it_dict[it_id] = kin_dict
+    data_dict[snap_id] = it_dict
+
+    return data_dict
